175b134671a8636c5b6dd8c3210a9affb47f2b2b
cowsoncows <coffedrop@live.com>
2021-11-12 21:17:40 -0500

    Added intro

--- src/ReplicatedFirst/main.client.lua
+++ src/ReplicatedFirst/main.client.lua
@@ -1,7 +1,182 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local player = Players.LocalPlayer
+local StarterGui = game:GetService("StarterGui")
+local ContentProvider = game:GetService("ContentProvider")
+local SoundService = game:GetService("SoundService")
+local Player = game:GetService("Players").LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local RunService = game:GetService("RunService")
 
-require(ReplicatedStorage:WaitForChild("MainModule")).init(Players.LocalPlayer.PlayerScripts:WaitForChild("ClientModules"))
-require(ReplicatedStorage:WaitForChild("MainModule")).onLoadFinished()
+ReplicatedFirst:RemoveDefaultLoadingScreen()
+
+local loadingUI = ReplicatedFirst:WaitForChild("Intro"):Clone()
+loadingUI.Parent = PlayerGui
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+
+local TweenService = game:GetService("TweenService")
+local loadables = {
+    loadingUI,
+    "rbxassetid://7384029571",
+    SoundService, 
+    "rbxassetid://474829770",
+    "rbxassetid://4919760420",
+    "rbxassetid://1835749037",
+    "rbxassetid://1841979451",
+    "rbxassetid://1837566969"
+}
+local loaded = false
+local loadedEvent = Instance.new("BindableEvent")
+coroutine.wrap(function()
+	for i = 1, #loadables do
+		ContentProvider:PreloadAsync({loadables[i]})
+	end
+	loaded = true
+    loadedEvent:Fire()
+end)()
+local moduleLoaded = false
+local moduleLoadedEvent = Instance.new("BindableEvent")
+coroutine.wrap(function()
+    require(ReplicatedStorage:WaitForChild("MainModule")).init(player.PlayerScripts:WaitForChild("ClientModules"))
+    moduleLoaded = true
+    moduleLoadedEvent:Fire()
+end)()
+
+-- begin intro
+
+local logo = loadingUI.Frame.logo
+local top = logo.top
+local bottom = logo.bottom
+local cover = loadingUI.Frame.black
+local text = loadingUI.Frame.title
+
+local sound = Instance.new("Sound", SoundService)
+sound.SoundId = "rbxassetid://7384029571"
+if not sound.IsLoaded then sound.Loaded:Wait() end
+
+local fadeInfo = TweenInfo.new(0.5, Enum.EasingStyle.Linear)
+
+local function openMouth(t)
+    local tween = TweenService:Create(
+        bottom,
+        TweenInfo.new(
+            t/2,
+            Enum.EasingStyle.Quad,
+            Enum.EasingDirection.InOut,
+            0,
+            true
+        ),
+        {
+            Position = UDim2.fromScale(0, 0.1)
+        }
+    )
+    tween:Play()
+    return tween
+end
+
+local function tiltHead(t, theta)
+    local tween = TweenService:Create(
+        logo,
+        TweenInfo.new(
+            t/2,
+            Enum.EasingStyle.Quad,
+            Enum.EasingDirection.InOut,
+            0,
+            true
+        ),
+        {
+            Rotation = theta,
+            Size = UDim2.fromScale(1.25, 1.25)
+        }
+    )   
+    tween:Play()
+    return tween
+end
+
+local function map(x, inMin, inMax, outMin, outMax)
+    return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+end
+
+
+local fadeTween = TweenService:Create(
+    cover,
+    fadeInfo,
+    {
+        BackgroundTransparency = 1
+    }
+)
+fadeTween:Play()
+fadeTween.Completed:Wait()
+
+sound:Play()
+
+tiltHead(0.2, 35)
+openMouth(0.2).Completed:Wait()
+
+tiltHead(0.2, -35)
+openMouth(0.2).Completed:Wait()
+
+tiltHead(0.3, 30)
+openMouth(0.3).Completed:Wait()
+
+tiltHead(0.3, -30)
+openMouth(0.3).Completed:Wait()
+
+tiltHead(0.3, 20)
+openMouth(0.3).Completed:Wait()
+
+tiltHead(0.3, -20)
+openMouth(0.3).Completed:Wait()
+
+tiltHead(0.75, 10)
+openMouth(0.75)
+
+for i = 1, 200 do
+    text.UIGradient.Transparency = NumberSequence.new({
+        NumberSequenceKeypoint.new(0, 0),
+        NumberSequenceKeypoint.new(i/200, 1),
+        NumberSequenceKeypoint.new(1, 1)
+    })
+    RunService.RenderStepped:Wait()
+end
+
+task.wait(1)
+
+if not loaded then loadedEvent.Event:Wait() end
+if not moduleLoaded then moduleLoadedEvent.Event:Wait() end
+if sound.IsPlaying then sound.Ended:Wait() end
+
+-- end intro
+
+
+local fadeOutTween = TweenService:Create(
+    cover,
+    fadeInfo,
+    {
+        BackgroundTransparency = 0
+    }
+)
+fadeOutTween:Play()
+fadeOutTween.Completed:Wait()
+
+logo.Visible = false
+
+local fadeOutTween2 = TweenService:Create(
+    loadingUI.Frame,
+    fadeInfo,
+    {
+        BackgroundTransparency = 1
+    }
+)
+fadeOutTween2:Play()
+fadeOutTween2.Completed:Wait()
+
+loadingUI:Destroy()
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, true)
+
+require(ReplicatedStorage.MainModule).onLoadFinished()
 
 
